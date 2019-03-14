7bff156d84e62f1ebcd8347b9b9c7be750f04792
AlinaT <atutuianu2017@fau.edu>
2019-03-14 13:38:31 -0400

    Added connection through bluetooth device in menu bar

--- app/src/main/java/com/example/uarmbt/child_calibration.java
+++ app/src/main/java/com/example/uarmbt/child_calibration.java
@@ -1,8 +1,11 @@
 package com.example.uarmbt;
 
+import android.app.Activity;
+import android.content.*;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,10 +16,6 @@ import android.widget.Toast;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.widget.AdapterView;
@@ -35,24 +34,32 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.ArrayList;
 
+import com.macroyau.blue2serial.BluetoothDeviceListDialog;
+import com.macroyau.blue2serial.BluetoothSerial;
+import com.macroyau.blue2serial.BluetoothSerialListener;
 
-public class child_calibration extends AppCompatActivity {
 
-    private ListView deviceList;
-    private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-    private ArrayAdapter arrayAdapter;
-     Set<BluetoothDevice> PairedDevices;
+public class child_calibration extends AppCompatActivity implements BluetoothSerialListener, BluetoothDeviceListDialog.OnDeviceSelectedListener {
 
-    private final static int REQUEST_ENABLE_BT = 1; // used to identify adding bluetooth names
+    private TextView textView;
+    private BluetoothSerial bluetoothSerial;
 
-    BluetoothSPP bt = new BluetoothSPP(Context);
+    private final static int REQUEST_ENABLE_BLUETOOTH = 1; // used to identify adding bluetooth names
+
+    private MenuItem actionConnect, actionDisconnect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_child_calibration);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        bluetoothSerial = new BluetoothSerial(this, this);
+
+        TextView textView = (TextView) findViewById(R.id.textView_calibration);
 
         Button button = (Button) findViewById(R.id.button_calibration);
 
@@ -70,11 +77,42 @@ public class child_calibration extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check Bluetooth availability on the device and set up the Bluetooth adapter
+        bluetoothSerial.setup();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Open a Bluetooth serial port and get ready to establish a connection
+        if (bluetoothSerial.checkBluetooth() && bluetoothSerial.isBluetoothEnabled()) {
+            if (!bluetoothSerial.isConnected()) {
+                bluetoothSerial.start();
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Disconnect from the remote device and close the serial port
+        bluetoothSerial.stop();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
+
+        actionConnect = menu.findItem(R.id.action_connect);
+        actionDisconnect = menu.findItem(R.id.action_disconnect);
 
         // return true so that the menu pop up is opened
         return true;
@@ -86,33 +124,11 @@ public class child_calibration extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_connect:
                 // User chose the "Connect" item
-                if (bluetoothAdapter == null) {
-                    // Device does not support Bluetooth
-                    Toast.makeText(getApplicationContext(),"Device does not support bluetooth!",Toast.LENGTH_SHORT).show();
-                }
-                if (!bluetoothAdapter.isEnabled()) {
-                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                    Toast.makeText(getApplicationContext(),"Bluetooth turned on",Toast.LENGTH_SHORT).show();
-                   /* Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-                    ArrayList list = new ArrayList();
-                    if(pairedDevices.size()>0){
-                        for(BluetoothDevice device: pairedDevices){
-                            String devicename = device.getName();
-                            String macAddress = device.getAddress();
-                            list.add("Name: "+devicename+"MAC Address: "+macAddress);
-                        }
-                        deviceList = (ListView) findViewById(R.id.deviceList);
-                        arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, list);
-                        deviceList.setAdapter(arrayAdapter);
-                    }*/
-                }
-                else {
-                    Toast.makeText(getApplicationContext(),"Bluetooth already On",Toast.LENGTH_SHORT).show();
+                showDeviceListDialog();
+                return true;
 
-                }
-
-
+            case R.id.action_disconnect:
+                bluetoothSerial.stop();
                 return true;
 
             case R.id.action_overflow:
@@ -127,17 +143,141 @@ public class child_calibration extends AppCompatActivity {
         }
     }
 
-   /* private void listPairedDevices(View view){
-        PairedDevices = bluetoothAdapter.getBondedDevices();
-        if(bluetoothAdapter.isEnabled()) {
-            // put it's one to the adapter
-            for (BluetoothDevice device : PairedDevices)
-                BTArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+    @Override
+    public void invalidateOptionsMenu() {
+        if (bluetoothSerial == null)
+            return;
 
-            Toast.makeText(getApplicationContext(), "Show Paired Devices", Toast.LENGTH_SHORT).show();
+        // Show or hide the "Connect" and "Disconnect" buttons on the app bar
+        if (bluetoothSerial.isConnected()) {
+            if (actionConnect != null)
+                actionConnect.setVisible(false);
+            if (actionDisconnect != null)
+                actionDisconnect.setVisible(true);
+        } else {
+            if (actionConnect != null)
+                actionConnect.setVisible(true);
+            if (actionDisconnect != null)
+                actionDisconnect.setVisible(false);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQUEST_ENABLE_BLUETOOTH:
+                // Set up Bluetooth serial port when Bluetooth adapter is turned on
+                if (resultCode == Activity.RESULT_OK) {
+                    bluetoothSerial.setup();
+                }
+                break;
+        }
+    }
+
+    private void updateBluetoothState() {
+        // Get the current Bluetooth state
+        final int state;
+        if (bluetoothSerial != null)
+            state = bluetoothSerial.getState();
         else
-            Toast.makeText(getApplicationContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
-    }*/
+            state = BluetoothSerial.STATE_DISCONNECTED;
+
+        // Display the current state on the app bar as the subtitle
+        String subtitle;
+        switch (state) {
+            case BluetoothSerial.STATE_CONNECTING:
+                subtitle = getString(R.string.status_connecting);
+                break;
+            case BluetoothSerial.STATE_CONNECTED:
+                subtitle = getString(R.string.status_connected, bluetoothSerial.getConnectedDeviceName());
+                break;
+            default:
+                subtitle = getString(R.string.status_disconnected);
+                break;
+        }
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setSubtitle(subtitle);
+        }
+    }
+
+    private void showDeviceListDialog() {
+        // Display dialog for selecting a remote Bluetooth device
+        BluetoothDeviceListDialog dialog = new BluetoothDeviceListDialog(this);
+        dialog.setOnDeviceSelectedListener(this);
+        dialog.setTitle(R.string.paired_devices);
+        dialog.setDevices(bluetoothSerial.getPairedDevices());
+        dialog.showAddress(true);
+        dialog.show();
+    }
+
+    /* Implementation of BluetoothSerialListener */
+
+    @Override
+    public void onBluetoothNotSupported() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.no_bluetooth)
+                .setPositiveButton(R.string.action_quit, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+
+    @Override
+    public void onBluetoothDisabled() {
+        Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBluetooth, REQUEST_ENABLE_BLUETOOTH);
+    }
+
+    @Override
+    public void onBluetoothDeviceDisconnected() {
+        invalidateOptionsMenu();
+        updateBluetoothState();
+    }
+
+    @Override
+    public void onConnectingBluetoothDevice() {
+        updateBluetoothState();
+    }
+
+    @Override
+    public void onBluetoothDeviceConnected(String name, String address) {
+        invalidateOptionsMenu();
+        updateBluetoothState();
+    }
+
+    @Override
+    public void onBluetoothSerialRead(String message) {
+        // Print the incoming message on the terminal screen
+        textView.append(getString(R.string.terminal_message_template,
+                bluetoothSerial.getConnectedDeviceName(),
+                message));
+        //svTerminal.post(scrollTerminalToBottom);
+    }
+
+    @Override
+    public void onBluetoothSerialWrite(String message) {
+        // Print the outgoing message on the terminal screen
+        textView.append(getString(R.string.terminal_message_template,
+                bluetoothSerial.getLocalAdapterName(),
+                message));
+        //svTerminal.post(scrollTerminalToBottom);
+    }
+
+    /* Implementation of BluetoothDeviceListDialog.OnDeviceSelectedListener */
+
+    @Override
+    public void onBluetoothDeviceSelected(BluetoothDevice device) {
+        // Connect to the selected remote Bluetooth device
+        bluetoothSerial.connect(device);
+    }
+
+    /* End of the implementation of listeners */
 
 }
